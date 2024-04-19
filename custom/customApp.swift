b91af90faae8de0a84ec700e2feab6c9f4007a8b
billhett <bill.hettinger@gmail.com>
2024-04-19 19:19:54 -0400

    Initial Commit

---
+++ custom/customApp.swift
@@ -0,0 +1,17 @@
+//
+//  customApp.swift
+//  custom
+//
+//  Created by William Hettinger on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct customApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
