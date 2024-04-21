43c9bf872a6675fe910f4be6fd851e77c2102818
billhett <bill.hettinger@gmail.com>
2024-04-21 19:17:25 -0400

    - initial commit

--- custom/ContentView.swift
+++ custom/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                // code here
+            } label: {
+                Text("Button 1")
+            }
+
         }
         .padding()
     }
---
+++ custom/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  custom
+//
+//  Created by William Hettinger on 4/19/24.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
