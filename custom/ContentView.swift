7ddad8f8eb1e44679041b33d27e9d8df553e9b04
billhett <bill.hettinger@gmail.com>
2024-04-22 21:48:04 -0400

    REFACTOR: update contentview

--- custom/ContentView.swift
+++ custom/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var heartRating = 0
     @State private var pinRating = 0
     @State private var bookMarkRating = 0
+    @State private var flagRating = 0
     
     var body: some View {
         RatingsView(
@@ -39,6 +40,13 @@ struct ContentView: View {
             //width: <#T##Int#>,
             symbolEnum: .bookmark,
             color: .teal
+        )
+        RatingsView(
+            maxRating: 7,
+            currentRating: $flagRating,
+            width: 30,
+            symbolEnum: .flag,
+            color: .green
         )
     }
 }
